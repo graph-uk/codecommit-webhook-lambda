bc37f48dd03123e042b79bc11260282095aaf64c
jincod <vadim.abdrashitov@gmail.com>
2016-09-28 14:27:33 +0500

    implemented lambda handler

--- codecommit-webhook.js
+++ codecommit-webhook.js
@@ -1,3 +1,33 @@
-exports.handler = function (event) {
-	console.log('Received event:', JSON.stringify(event, null, 2));
+const aws = require('aws-sdk');
+const https = require('https');
+
+const config = require('./config.json');
+const codecommit = new aws.CodeCommit();
+
+const sendPushNotification = (url, branches) => {
+	const path = `/git/notifyCommit?url=${url}&branches=${branches.join(',')}`;
+
+	return new Promise((resolve, reject) => {
+		https.get(`${config.jenkinsUrl}${path}`, res => {
+			if (res.statusCode === 200) {
+				resolve();
+			} else {
+				reject();
+			}
+
+			res.resume();
+		}).on('error', reject);
+	});
+}
+
+exports.handler = event => {
+	const data = JSON.parse(event.Records[0].Sns.Message).Records[0];
+	const repositoryName = data.eventSourceARN.split(':').pop();
+	const branches = data.codecommit.references.map(r => r.ref);
+
+	return codecommit.getRepository({repositoryName})
+		.promise()
+		.then(data => data.repositoryMetadata.cloneUrlSsh)
+		.then(url => sendPushNotification(url, branches))
+		.catch(console.error);
 };
