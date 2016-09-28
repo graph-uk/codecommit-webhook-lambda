4aeff1f1e94bc1586deeba9ef5ae1aeb7b4db8ab
jincod <vadim.abdrashitov@gmail.com>
2016-09-28 10:14:38 +0500

    rename resources

--- main.tf
+++ main.tf
@@ -9,30 +9,30 @@ resource "aws_iam_role" "iam_for_lambda" {
     assume_role_policy = "${file("${path.module}/assumeRolePolicy.json")}"
 }
 
-resource "aws_sns_topic" "webhook-sns" {
-	name = "webhook-sns-topic"
+resource "aws_sns_topic" "webhook_sns" {
+	name = "webhook_sns_topic"
 }
 
-resource "aws_lambda_function" "webhook-lambda" {
-	filename = "codecommit-webhook-payload.zip"
+resource "aws_lambda_function" "webhook_lambda" {
+	filename = "codecommit-webhook.zip"
 	function_name = "codecommit-webhook"
 	role = "${aws_iam_role.iam_for_lambda.arn}"
 	handler = "codecommit-webhook.handler"
 	runtime = "nodejs4.3"
-	source_code_hash = "${base64sha256(file("codecommit-webhook-payload.zip"))}"
+	source_code_hash = "${base64sha256(file("codecommit-webhook.zip"))}"
 }
 
-resource "aws_sns_topic_subscription" "webhook-sns-subscription" {
-	depends_on = ["aws_lambda_function.webhook-lambda"]
-	topic_arn = "${aws_sns_topic.webhook-sns.arn}"
+resource "aws_sns_topic_subscription" "webhook_sns_subscription" {
+	depends_on = ["aws_lambda_function.webhook_lambda"]
+	topic_arn = "${aws_sns_topic.webhook_sns.arn}"
 	protocol = "lambda"
-	endpoint = "${aws_lambda_function.webhook-lambda.arn}"
+	endpoint = "${aws_lambda_function.webhook_lambda.arn}"
 }
 
-resource "aws_lambda_permission" "with_sns" {
+resource "aws_lambda_permission" "allow_from_sns" {
 	statement_id = "AllowExecutionFromSNS"
 	action = "lambda:InvokeFunction"
-	function_name = "${aws_lambda_function.webhook-lambda.arn}"
+	function_name = "${aws_lambda_function.webhook_lambda.arn}"
 	principal = "sns.amazonaws.com"
-	source_arn = "${aws_sns_topic.webhook-sns.arn}"
+	source_arn = "${aws_sns_topic.webhook_sns.arn}"
 }
