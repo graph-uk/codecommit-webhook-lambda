aa9220d74484271f687cd5f1edace23a56039c78
jincod <vadim.abdrashitov@gmail.com>
2016-09-28 13:15:48 +0500

    added role policy

--- main.tf
+++ main.tf
@@ -4,9 +4,15 @@ provider "aws" {
 	region = "us-east-1"
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-    name = "iam_for_lambda"
-    assume_role_policy = "${file("${path.module}/assumeRolePolicy.json")}"
+resource "aws_iam_role" "iam_for_webhook-lambda" {
+    name = "iam_for_webhook-lambda"
+    assume_role_policy = "${file("${path.module}/policies/assumeRolePolicy.json")}"
+}
+
+resource "aws_iam_role_policy" "iam_policy_for_webhook_lambda" {
+    name = "iam_policy_for_webhook_lambda"
+	role = "${aws_iam_role.iam_for_webhook-lambda.id}"
+    policy = "${file("${path.module}/policies/assumePolicy.json")}"
 }
 
 resource "aws_sns_topic" "webhook_sns" {
@@ -16,7 +22,7 @@ resource "aws_sns_topic" "webhook_sns" {
 resource "aws_lambda_function" "webhook_lambda" {
 	filename = "codecommit-webhook.zip"
 	function_name = "codecommit-webhook"
-	role = "${aws_iam_role.iam_for_lambda.arn}"
+	role = "${aws_iam_role.iam_for_webhook-lambda.arn}"
 	handler = "codecommit-webhook.handler"
 	runtime = "nodejs4.3"
 	source_code_hash = "${base64sha256(file("codecommit-webhook.zip"))}"
